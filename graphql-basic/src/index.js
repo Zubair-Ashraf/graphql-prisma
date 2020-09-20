881279c61cb8a4b13a46569f4ce2d261183b76c2
zubair-ashraf <zubair8767844@gmail.com>
2020-09-20 16:32:02 +0100

    Working on Arrays

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,9 +1,24 @@
 import { GraphQLServer } from "graphql-yoga";
 
+//Dummy Data
+const users = [
+  { id: 1, name: "Zubair", email: "zubair@gmail.com", age: 26 },
+  { id: 2, name: "Bilal", email: "bilal@gmail.com", age: 26 },
+  { id: 3, name: "Ayaan", email: "ayaan@gmail.com", age: 5 },
+];
+
 //Type defination (schema)
 const typeDefs = `
     type Query {
-        greeting(name: String): String!
+      greeting(name: String): String!
+      add(numbers: [Float!]!): Float!
+      users(query :String): [User!]!
+    }
+    type User {
+      id: ID!
+      name: String!
+      email: String!
+      age: Int
     }
 `;
 
@@ -13,6 +28,19 @@ const resolvers = {
     greeting(parent, args, ctx, info) {
       const { name } = args;
       return `Hello ${name}`;
+    },
+    add(parent, { numbers }, ctx, info) {
+      return numbers.reduce((a, c) => {
+        return a + c;
+      });
+    },
+    users(parent, { query }) {
+      if (!query) return users;
+      else {
+        return users.filter((user) =>
+          user.name.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+        );
+      }
     },
   },
 };
