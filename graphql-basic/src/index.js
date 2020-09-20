3e154c951c22ae33fae9e60703cf0f69488788fd
zubair-ashraf <zubair8767844@gmail.com>
2020-09-20 22:12:46 +0100

    create-user, create-posts, create-comments mutation

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,54 +1,55 @@
 import { GraphQLServer } from "graphql-yoga";
+import uuid from "uuid/v4";
 
 //Dummy Data
 const users = [
-  { id: 1, name: "Zubair", email: "zubair@gmail.com", age: 26 },
-  { id: 2, name: "Bilal", email: "bilal@gmail.com", age: 26 },
-  { id: 3, name: "Ayaan", email: "ayaan@gmail.com", age: 5 },
+  { id: "1", name: "Zubair", email: "zubair@gmail.com", age: 26 },
+  { id: "2", name: "Bilal", email: "bilal@gmail.com", age: 26 },
+  { id: "3", name: "Ayaan", email: "ayaan@gmail.com", age: 5 },
 ];
 
 const posts = [
   {
-    id: 1,
+    id: "1",
     title: "GraphQL",
     body: "This post is for graphql",
     published: true,
-    author: 1,
+    author: "1",
   },
   {
-    id: 2,
+    id: "2",
     title: "React.js",
     body: "This post is for reactjs",
     published: false,
-    author: 1,
+    author: "1",
   },
   {
-    id: 3,
+    id: "3",
     title: "Node.js",
     body: "This post is for nodejs",
     published: true,
-    author: 2,
+    author: "2",
   },
 ];
 
 const comments = [
   {
-    id: 1,
+    id: "1",
     text: "This lecture is so tuff",
-    author: 1,
-    post: 1,
+    author: "1",
+    post: "1",
   },
   {
-    id: 2,
+    id: "2",
     text: "React js is so awesome",
-    author: 2,
-    post: 3,
+    author: "2",
+    post: "3",
   },
   {
-    id: 3,
+    id: "3",
     text: "Node js comment",
-    author: 3,
-    post: 3,
+    author: "3",
+    post: "3",
   },
 ];
 
@@ -60,6 +61,11 @@ const typeDefs = `
       users(query :String): [User!]!
       posts(query: String): [Post!]!
       comments: [Comment!]!
+    }
+    type Mutation {
+      createUser(name: String!, email: String!, age: Int): User!
+      createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+      createComment(text: String!, author: ID!, post: ID!): Comment!
     }
     type User {
       id: ID!
@@ -121,8 +127,59 @@ const resolvers = {
       return comments;
     },
   },
+  Mutation: {
+    createUser(parent, { name, email, age }) {
+      const emailExist = users.some((user) => user.email === email);
+      if (emailExist) throw new Error("Email already registered");
+      const user = {
+        id: uuid(),
+        name,
+        email,
+        age,
+      };
+      users.push(user);
+      return user;
+    },
+    createPost(parent, { title, body, published, author }) {
+      const userExist = users.some((user) => user.id === author);
+      if (!userExist) throw new Error("User doesn't exist");
+      const post = {
+        id: uuid(),
+        title,
+        body,
+        published,
+        author,
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+    createComment(parent, { text, author, post: postId }) {
+      const userExist = users.some((user) => user.id === author);
+      if (!userExist) throw new Error("User doesn't exist");
+
+      const postExist = posts.some(
+        (post) => post.id === postId && post.published
+      );
+
+      if (!postExist) throw new Error("Post doesn't exist");
+
+      const comment = {
+        id: uuid(),
+        text,
+        author,
+        post: postId,
+      };
+
+      comments.push(comment);
+
+      return comment;
+    },
+  },
   Post: {
     author(parent, args, ctx, info) {
+      // console.log(parent);
       return users.find((user) => {
         return user.id === parent.author;
       });
